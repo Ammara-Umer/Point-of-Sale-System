d783f63f5c3d6471fb95fd121c67fd9e00dc027a
Ammara-Umer <ammara.umer@rolustech.com>
2017-01-19 17:41:01 +0500

    Delete NotFoundController.php

--- Controllers/NotFoundController.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include 'Interfaces/IController';
-class NotFoundController implements IController{
-    public function index() {
-        require_once 'Views/Errors/NotFoundController.php';
-    }
-}
-
