b8f449e2e3dc037252d143d68863427f422a42bc
Ammara-Umer <ammara.umer@rolustech.com>
2017-01-19 17:46:47 +0500

    Delete SourceFile.php

--- Sources/SourceFile.php
+++
@@ -1,5 +0,0 @@
-<!--file to include all scripts and css -->
-
-<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
-<script src="js/bootstrap.min.js" type="text/javascript"></script>
-<script src="js/jquery.js" type="text/javascript"></script>
